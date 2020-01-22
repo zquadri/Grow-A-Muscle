18380a06f6634239c40054d368730c882f607809
Zaid Quadri <zquadri98@gmail.com>
2020-01-21 23:36:55 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let beginnerDescript = document.querySelector(".beginner-description");
+let interDescript = document.querySelector(".intermediate-description");
+let expertDescript = document.querySelector(".expert-description");
+
+console.log(beginnerDescript);
+console.log(interDescript);
+console.log(expertDescript);
+
+let headerText = document.querySelector(".level-header");
+
+//localeUpperCase??
+// headerText.textContent = headerText.textContent.toUpperCase();
